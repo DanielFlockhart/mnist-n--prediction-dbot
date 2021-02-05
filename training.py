09394b5e222080035896e2be8e240dd1882516e6
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2021-02-05 13:51:58 +0000

    Update training.py

--- training.py
+++ training.py
@@ -6,22 +6,41 @@ import time
 (x_train,y_train),(x_test,y_test)= tf.keras.datasets.mnist.load_data()
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
-
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+'''
 model = tf.keras.models.Sequential()
 
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
-
+'''
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16, kernel_size=3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=2),
+    tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+    ])
 EPOCHS = 20
 
-early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 
 model.compile(optimizer="adam",
               loss="sparse_categorical_crossentropy",
               metrics=['accuracy'])
-model.fit(x_train,y_train,epochs=EPOCHS ,callbacks=[early_stopping_callback])
+
+history = model.fit(x_train,y_train,epochs=EPOCHS)
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+
 val_loss, val_acc = model.evaluate(x_test,y_test)
 m_name = "mnist_numbers.model"
 model.save(m_name)
