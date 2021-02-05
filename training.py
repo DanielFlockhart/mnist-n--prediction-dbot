4628a8f51133efbaa47bc1007817f590d1bf6ceb
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2021-02-05 15:16:18 +0000

    Update training.py

--- training.py
+++ training.py
@@ -17,15 +17,17 @@ model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
 '''
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(16, kernel_size=3, activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=2),
     tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
     tf.keras.layers.MaxPooling2D(pool_size=2),
+    tf.keras.layers.Conv2D(64, kernel_size=3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=2),
+    tf.keras.layers.Conv2D(128, kernel_size=3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=2),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
     ])
-EPOCHS = 20
+EPOCHS = 10
 
 
 model.compile(optimizer="adam",
